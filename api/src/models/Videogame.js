11d652788fed5873e4f2af3349467d5f5f8708e2
matirozas <matiasrozas.lp@gmail.com>
2023-03-28 11:58:53 -0300

    modelos

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -1,47 +1,46 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('videogame', {
-    
-    id:{                              
-      type:DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      allownull: false,
-      primaryKey: true
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },                            
-    description:{
-      type:DataTypes.TEXT,
-      allowNull:false,
-    },
-    released:{
-      type: DataTypes.STRING,
-    
-    },
-    rating:{
-      type:DataTypes.FLOAT
+  sequelize.define(
+    "videogame",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allownull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      released: {
+        type: DataTypes.STRING,
+      },
+      rating: {
+        type: DataTypes.FLOAT,
+      },
 
+      genres: {
+        type: DataTypes.ARRAY(DataTypes.STRING),
+        allowNull: false,
+      },
+      platforms: {
+        type: DataTypes.ARRAY(DataTypes.STRING),
+        allowNull: false,
+      },
+      background_image: {
+        type: DataTypes.STRING,
+      },
     },
-    platforms:{
-      type:DataTypes.ARRAY(DataTypes.STRING),
-      allowNull:false
-    },
-    background_image:{
-      type:DataTypes.STRING
-    },
-    genres: { 
-      type:DataTypes.STRING,
-      allowNull:false
+    {
+      timestamps: false,
     }
-   
-      
-   
-  },{
-  timestamps:false
-});
+  );
 };
